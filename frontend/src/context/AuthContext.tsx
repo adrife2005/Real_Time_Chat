0d64d2f1faa1e23c57fc586b435f1cb586995e97
AdrifeCova <adrife2000@hotmail.com>
2024-12-29 12:18:17 -0600

    feat: authContext added

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useState, useEffect, useContext } from "react";
+
+type AuthUserType = {
+  id: string;
+  fullName: string;
+  email: string;
+  profilePic: string;
+  gender: string
+}
+
+interface AuthContextType {
+  authUser: AuthUserType | null;
+  setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  authUser: null,
+  setAuthUser: () => { },
+  isLoading: true,
+})
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+  return useContext(AuthContext);
+}
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [authUser, setAuthUser] = useState<AuthUserType | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch('/api/auth/me')
+
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.message);
+        }
+
+        setAuthUser(data);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{
+      authUser,
+      isLoading,
+      setAuthUser,
+    }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
