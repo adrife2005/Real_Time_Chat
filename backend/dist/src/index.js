253b482a2be58c068f77c38939d6b05d4a459585
AdrifeCova <adrife2000@hotmail.com>
2025-01-01 18:07:03 -0600

    feat: npm run build

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -10,12 +10,20 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const message_route_1 = __importDefault(require("./routes/message.route"));
 const socket_1 = require("./socket/socket");
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const PORT = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : 3001;
+const __dirname = path_1.default.resolve();
 socket_1.app.use((0, cookie_parser_1.default)());
 socket_1.app.use(express_1.default.json());
 socket_1.app.use('/api/auth', auth_route_1.default);
 socket_1.app.use('/api/messages', message_route_1.default);
+if (process.env.NODE_ENV === 'production') {
+    socket_1.app.use(express_1.default.static(path_1.default.join(__dirname, '../frontend/build')));
+    socket_1.app.get('*', (_req, res) => {
+        res.sendFile(path_1.default.join(__dirname, '../frontend/build/index.html'));
+    });
+}
 socket_1.server.listen(PORT, () => {
     console.log('Server is running on http://localhost:' + PORT);
 });
